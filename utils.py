cd89dbba7e9fdc1c0595282cb30385a31cfa2fa8
prady39 <pradneshchavan@gmail.com>
2020-08-23 19:48:12 +0530

    Added utils.py file

---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+has_proxy = False
+proxies = {}
+try:
+    proxies = {
+        'http': os.environ['http_proxy'],
+        'https': os.environ['https_proxy']
+    }
+    has_proxy = True
+    print "Proxy detected\n"
+except KeyError:
+    pass
+
+
+def scrape(url):
+    response = requests.get(
+        url, proxies=proxies) if has_proxy else requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html)
+    orgs = soup.findAll('li', attrs={'class': 'organization-card__container'})
+    file = open('orgs.txt', 'w')
+    for org in orgs:
+        org_name = org.find('h4').text
+        print org_name
+        org_name = org_name.encode('utf-8')
+        file.write(org_name+'\n')
+    file.close()
